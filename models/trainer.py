bfbd270f9fcdaac6e5f97c2d3201f6521111af45
Zongyu Yin <zongyu.yin@outlook.com>
2023-02-12 20:52:05 +0000

    feat: added training resumption and model inference

--- models/trainer.py
+++ models/trainer.py
@@ -1,6 +1,7 @@
 import time
 import os
 import errno
+from typing import Optional
 import datetime
 from hyperpyyaml import load_hyperpyyaml
 from pathlib import Path
@@ -36,13 +37,31 @@ class TrainerBase:
 
     def __init__(
         self,
-        cfg_path,
-        device
+        device,
+        cfg_path: Optional[str] = None,
+        ckpt_path: Optional[str] = None
     ):
+        if cfg_path is None and ckpt_path is None:
+            raise ValueError(
+                "Need either cfg_path (start training) or ckpt_path (resume training)."
+            )
+        elif cfg_path is not None:
+            timestamp = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+            self.cfg_path = cfg_path
+            self.logdir = os.path.join("experiments", timestamp)
+            copyfile(self.cfg_path, os.path.join(self.logdir, "config.yaml"))
+        elif ckpt_path is not None:
+            ckpt_dir = os.path.dirname(ckpt_path)
+            self.cfg_path = os.path.join(ckpt_dir, "config.yaml")
+            self.logdir = ckpt_dir
+        
+        self.writer = SummaryWriter(self.logdir)
+        
         self.device = device
-        with open(cfg_path) as f:
+        with open(self.cfg_path) as f:
             self.cfg = load_hyperpyyaml(f)
         self.model = self.cfg["model"].to(self.device)
+
         train_data = Dataset(
             self.cfg["data_params"]["data_dir"],
             "train",
@@ -69,10 +88,6 @@ class TrainerBase:
             num_workers=0,
             collate_fn=CollateBatch(device=self.device)
         )
-        timestamp = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-        self.logdir = os.path.join("experiments", timestamp)
-        self.writer = SummaryWriter(self.logdir)
-        copyfile(cfg_path, os.path.join(self.logdir, "config.yaml"))
         opt = optim.Adam(
             self.model.parameters(),
             lr=0.00001,
@@ -89,36 +104,41 @@ class TrainerBase:
         self.end_epoch = self.cfg["train_params"]["epoch"]
         self.save_step = self.cfg["train_params"]["save_step"]
 
+        if ckpt_path is not None:
+            self.resume(ckpt_path)
+
     def resume(self, ckpt_path: str):
         ckpt = torch.load(ckpt_path, map_location=self.device)
         self.scheduler.load_state_dict(ckpt["optimizer_state_dict"])
         self.model.load_state_dict(ckpt["model_state_dict"])
         self.start_epoch = ckpt["epoch"] + 1
 
-    def loss(self, batch):
+    def loss(self):
         raise NotImplementedError
 
-    def train(self, ckpt_path: str):
+    def train(self):
         raise NotImplementedError
 
-    def generate(self, ckpt_path: str):
+    def generate(self):
         raise NotImplementedError
 
 
 class MTTrainer(TrainerBase):
 
-    def __init__(self, cfg_path, device):
-        super().__init__(cfg_path, device)
+    def __init__(
+        self,
+        device,
+        cfg_path: Optional[str] = None,
+        ckpt_path: Optional[str] = None
+    ):
+        super().__init__(device, cfg_path, ckpt_path)
         self.loss_func = nn.CrossEntropyLoss(ignore_index=0, reduction='sum')
 
     def loss(self, batch, recon_batch):
         ce = self.loss_func(recon_batch, batch)
         return ce
 
-    def train(self, ckpt_path: str = None):
-        if ckpt_path is not None:
-            self.resume(ckpt_path)
-
+    def train(self):
         for e in range(self.start_epoch, self.end_epoch):
             ##################
             #     Train      #
@@ -126,7 +146,7 @@ class MTTrainer(TrainerBase):
             self.model.train()
             with tqdm(self.train_loader) as t:
                 t.set_description(f'Epoch {e}')
-                for batch in t:
+                for i, batch in enumerate(t):
                     recon_batch = self.model(batch[:, :-1])
                     loss = self.loss(batch[:, 1:], recon_batch)
 
@@ -141,8 +161,8 @@ class MTTrainer(TrainerBase):
                         batch[:, 1:]
                     ).sum().item() / norm * 100.
 
-                    self.writer.add_scalar('TRAIN/loss', loss_norm)
-                    self.writer.add_scalar('TRAIN/accuracy', acc_norm)
+                    self.writer.add_scalar('TRAIN/loss', loss_norm, e * len(self.train_loader) + i)
+                    self.writer.add_scalar('TRAIN/accuracy', acc_norm, e * len(self.train_loader) + i)
                     t.set_postfix(loss=loss_norm, accuracy=acc_norm)
 
             ##################
@@ -150,9 +170,9 @@ class MTTrainer(TrainerBase):
             ##################
             self.model.eval()
             with torch.no_grad():
-                for batch in self.valid_loader:
+                for i, batch in enumerate(self.valid_loader):
                     recon_batch = self.model(batch[:, :-1])
-                    loss = self.loss(batch, recon_batch)
+                    loss = self.loss(batch[:, 1:], recon_batch)
 
                     norm = (batch != 0).sum().item()
                     loss_norm = loss.item() / norm
@@ -160,8 +180,8 @@ class MTTrainer(TrainerBase):
                         recon_batch.max(1)[1],
                         batch[:, 1:]
                     ).sum().item() / norm * 100.
-                    self.writer.add_scalar('VALID/loss', loss_norm)
-                    self.writer.add_scalar('VALID/accuracy', acc_norm)
+                    self.writer.add_scalar('VALID/loss', loss_norm, e * len(self.train_loader) + i)
+                    self.writer.add_scalar('VALID/accuracy', acc_norm, e * len(self.train_loader) + i)
 
             ##################
             #      Save      #
@@ -182,8 +202,8 @@ class MTTrainer(TrainerBase):
         num_excerpts: int,
         length: int,
         save_dir: str,
-        time_quantization: list,
-        time_unit: str,
+        time_quantization: list = CSQ_TIME_QUANTIZATION,
+        time_unit: str = "crotchet",
         tempo: int = 120
     ):
         processor = MIDI(
@@ -197,7 +217,7 @@ class MTTrainer(TrainerBase):
                 torch.tensor(CSQ_START_DISTRIBUTION, device=self.device)
             ).sample(sample_shape=[num_excerpts, 1])
             outputs = self.model.decode(
-                z, length=length).unsqueeze(1).cpu().numpy()
+                z, length=length).cpu().numpy()
         for i in range(num_excerpts):
             mid = processor.to_midi([outputs[i]], tempo=tempo)
             mid.write(os.path.join(save_dir, f'{i}.mid'))
@@ -205,8 +225,13 @@ class MTTrainer(TrainerBase):
 
 class MVAETrainer(TrainerBase):
 
-    def __init__(self, cfg_path, device):
-        super().__init__(cfg_path, device)
+    def __init__(
+        self,
+        device,
+        cfg_path: Optional[str] = None,
+        ckpt_path: Optional[str] = None
+    ):
+        super().__init__(device, cfg_path, ckpt_path)
         self.loss_func = nn.CrossEntropyLoss(ignore_index=0, reduction='sum')
         self.kld_weight = 0
 
@@ -222,10 +247,7 @@ class MVAETrainer(TrainerBase):
         kld = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
         return ce, kld
 
-    def train(self, ckpt_path: str = None):
-        if ckpt_path is not None:
-            self.resume(ckpt_path)
-
+    def train(self):
         for e in range(self.start_epoch, self.end_epoch):
             ##################
             #     Train      #
@@ -312,7 +334,7 @@ class MVAETrainer(TrainerBase):
         with torch.no_grad():
             z = torch.rand(num_excerpts, self.cfg['hidden_size'] * 2).to(self.device)
             outputs = self.model.decode(
-                z, length=length).unsqueeze(1).cpu().numpy()
+                z, length=length).cpu().numpy()
         for i in range(num_excerpts):
             mid = processor.to_midi([outputs[i]], tempo=tempo)
             mid.write(os.path.join(save_dir, f'{i}.mid'))
